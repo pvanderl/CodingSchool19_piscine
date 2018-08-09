bff6620ace22c50d6419f7ffb84c9bf5540055a1
CinelRim <42189587+CinelRim@users.noreply.github.com>
2018-08-09 15:53:00 +0200

    Update ft_putnbr.c
    Version finie et fonctionelle

--- Day02/ex06/ft_putnbr.c
+++ Day02/ex06/ft_putnbr.c
@@ -1,3 +1,15 @@
-void ft_putnbr(int nb){
-  ft_putchar(48+nb);
+void ft_putnbr(int nb)
+{
+    int nombre;
+    nombre = nb;
+    if(nombre < 0)
+    {
+        ft_putchar('-');
+        nombre=-nombre;
+    }
+    if(nombre > 10)
+    {
+        ft_putnbr(nombre / 10);
+    }
+    ft_putchar('0' + (nombre % 10));
 }
